3e3ea1aee2bc929b4f690a87b54a37c51602ca79
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 12:39:14 -0700

    cp

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - entry point
  *
  * description: print a num and it last digit 
- * lastdigit is 0,> 5, or < 6 and not 0.
+ * last digit is 0 > 5, or < 6 and not 0.
  * Return: 0
  */
 int main(void)
@@ -15,7 +15,7 @@ int n;
 
 srand(time(0);
 n = rand() - RAND_MAX / 2;
-printf("last digit of %D %s\n", n, n % 10,
+printf("last digit of %d %s\n", n, n % 10,
 ((n % 10) == 0) ? "and is 0"
 : (((n % 10) > 5) ? "and is greater than 5"
 : "and is less than 6 and not 0"));
