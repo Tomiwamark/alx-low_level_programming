1336c398652946ad53f8e8784639160feb2ba897
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 12:15:30 -0700

    comr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - prints a random number and states whether it is positive, negative, or zero.
+ * main - prints a random number and states whether it is +ve, -ve, or zero.
  *
  * Return: Always 0.
  */
