debfe21dcd641dd74c346dfb556859bed8bb7e70
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 11:49:19 -0700

    git commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,4 +3,24 @@
 #include <stdio.h>
 
 /**
-*
+ * main - prints a random number and states whether it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 1)
+printf("%d is positive\n", n);
+else if (n < o)
+printf("%d is negative\n", n);
+else
+
+printf("%d is zero\n", n);
+
+return (0);
+}
