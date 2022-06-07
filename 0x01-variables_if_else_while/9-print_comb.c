571e58691fb90662ab95dfce63592b1ee244f676
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 19:32:56 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+
+{
+int n;
+for (n = 48; <= 57; n++)
+{
+putchar(n);
+
+if (n == 57)
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+} 
+putchar('\n');
+
+return (0)
+
+}
+
