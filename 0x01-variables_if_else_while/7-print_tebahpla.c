ff844bbe0d1eba39ae2e49d72b12c255e6268eb2
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 18:31:44 -0700

    d

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char l;
+for (l = 122; l >= 97; l--)
+{
+putchar(l);
+}
+
+putchar('\n');
+
+return (0);
+
+}
