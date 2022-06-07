a10d6aacfcfe823c448c4f80f64a670b5045e4cf
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 18:43:52 -0700

    y

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - entry point
+ *
+ * return: 0
+ */
+int main(void)
+{
+char b;
+for (b = 48; b <= 57; b ==)
+{
+putchar(b);
+}
+for (b = 97; b <= 102; b++)
+{
+putchar(b);
+}
+putchar ('\n');
+
+return (0);
+}
