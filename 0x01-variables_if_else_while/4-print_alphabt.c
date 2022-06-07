a4f9ad373d617bdd144073a2c8edf61b7d2f604f
Tomiwamark <Tomiwa236@gmial.com>
2022-06-06 17:26:12 -0700

    v

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+if (c == 'q')
+continue;
+
+else if (c == 'e')
+continue;
+
+putchar(c)
+
+putchar('\n');
+
+return (0);
+
+}
